34d323f8764272a236a5b3fefc12db2224002db6
searchone <plo.cryptocartel@protonmail.com>
2023-03-28 15:29:51 -0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,142 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+  const [retry, setRetry] = useState(0);
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
+  // create onChange function
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+  // create generateAction function
+  const generateAction = async () => {
+    console.log('Generating...');
+  
+    // Add this check to make sure there is no double click
+    if (isGenerating && retry === 0) return;
+  
+    // Set loading has started
+    setIsGenerating(true);
+  
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+  
+      setRetry(0);
+    }
+  
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+  
+    const data = await response.json();
+  
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+  
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      // Stop loading
+      setIsGenerating(false);
+      return;
+    }
+  
+    // Set final prompt here
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput('');
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+
+  // Create useEffect hook
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator | saerchone</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>stablediffused</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>type in the prompt 'search1nedev' example: search1nedev as a jedi</h2>
+          </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            <input className="prompt-box" value={ input } onChange={ onChange } />
+            {/* Add your prompt button in the prompt container */}
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={ generateAction }>
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{ finalPrompt }</p>
+          </div>
+          
+        )}
+  
       </div>
       <div className="badge-container grow">
         <a
